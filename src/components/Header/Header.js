6eb93d1995f2415b09ae8d389f33b26de7f49ff0
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-01 00:17:15 +0600

    add header component and customlink

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <nav className='navigation-container'>
+            <h1>Welcome T-shirt Mania!!!</h1>
+            <div className='navigation-link'>
+                <CustomLink to={'/'}>Home</CustomLink>
+                <CustomLink to={'/shop'}>Shop</CustomLink>
+                <CustomLink to={'/order'}>Order</CustomLink>
+                <CustomLink to={'/order-review'}>Order Review</CustomLink>
+                <CustomLink to={'/about'}>About</CustomLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
